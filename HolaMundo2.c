bf74786d2f3d059de3aba9482ac1c9f65217df51
sofiaCorzas <72525164+sofiaCorzas@users.noreply.github.com>
2020-10-07 16:52:39 -0500

    agregue algo

--- HolaMundo2.c
+++ HolaMundo2.c
@@ -4,5 +4,6 @@ printf("\nFernando Julian Diaz Vazquez\n");
 printf("Este es mi segundo codigo");
 printf("Programar es facil");
 printf("Voy a compartir con mi compañero el codigo");
+Printf("\n hola\n")
 return 0;
 }
