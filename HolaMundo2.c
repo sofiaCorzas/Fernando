f33acfcf8f42685b9fde8eacccd7499084dfb386
fernandodiaz1225 <60375614+fernandodiaz1225@users.noreply.github.com>
2020-10-07 16:18:28 -0500

    Código de la actividad 5

---
+++ HolaMundo2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+printf("\nFernando Julian Diaz Vazquez\n");
+printf("Este es mi segundo codigo");
+printf("Programar es facil");
+printf("Voy a compartir con mi compañero el codigo");
+return 0;
+}
